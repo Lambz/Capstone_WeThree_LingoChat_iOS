6e7076f59f4858e36c712361e8a981f8f5ce475d
Chetan Singh <chetan.singh.0602@gmail.com>
2020-08-09 23:57:24 -0400

    finished login screen

--- Lingo Chat/Controllers/LoginAndSignup/LoginViewController.swift
+++ Lingo Chat/Controllers/LoginAndSignup/LoginViewController.swift
@@ -10,21 +10,90 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var gmailButton: UIButton!
+    @IBOutlet weak var facebookButton: UIButton!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
-    */
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
+    private func setupViews() {
+        setupFieldLines()
+        setupFieldIcons()
+        setupButtonsShadow()
+    }
+    
+    
 }
+
+//implements UI methods
+extension LoginViewController {
+    private func setupFieldLines() {
+        emailField.setBottomBorder()
+        passwordField.setBottomBorder()
+    }
+    
+    private func setupFieldIcons() {
+        let accountImg = UIImage(named: "account")
+        emailField.setLeftIcon(accountImg!)
+        
+        let passwordImg = UIImage(named: "password")
+        passwordField.setLeftIcon(passwordImg!)
+    }
+    
+    private func setupButtonsShadow() {
+        setButtonShadow(button: facebookButton)
+        setButtonShadow(button: gmailButton)
+        setButtonShadow(button: loginButton)
+    }
+    
+    private func setButtonShadow(button: UIButton) {
+        button.layer.shadowColor = #colorLiteral(red: 0.03921568627, green: 0.5176470588, blue: 1, alpha: 1)
+        button.layer.shadowOffset = CGSize(width: 0, height: 0)
+        button.layer.shadowRadius = 8
+        button.layer.shadowOpacity = 0.2
+    }
+}
+
+//implements UI textfield image methods
+extension UITextField {
+
+    func setLeftIcon(_ icon: UIImage) {
+
+        let padding = 8
+        let size = 20
+
+        let outerView = UIView(frame: CGRect(x: 0, y: 0, width: size+padding, height: size) )
+        let iconView  = UIImageView(frame: CGRect(x: padding, y: 0, width: size, height: size))
+        iconView.image = icon
+        outerView.addSubview(iconView)
+
+        leftView = outerView
+        leftViewMode = .always
+    }
+    
+    func setBottomBorder() {
+        self.borderStyle = .none
+        self.layer.backgroundColor = UIColor.white.cgColor
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = UIColor.gray.cgColor
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+        self.layer.shadowOpacity = 1.0
+        self.layer.shadowRadius = 0.0
+    }
+}
+
+
