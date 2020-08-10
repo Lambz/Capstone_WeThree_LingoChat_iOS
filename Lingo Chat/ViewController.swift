094c64abc262e20a283588eb24a152e05876ad4d
Chetan Singh <chetan.singh.0602@gmail.com>
2020-08-09 20:04:38 -0400

    Initial Commit

---
+++ Lingo Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lingo Chat
+//
+//  Created by Chetan on 2020-08-09.
+//  Copyright © 2020 Chetan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
