17e09359b325af9bf78298de3cf9827b918c77b1
Chetan Singh <chetan.singh.0602@gmail.com>
2020-08-09 20:17:35 -0400

    Added basic file structure | Added basic configurations

--- Lingo Chat/Controllers/ChatsViewController.swift
+++ Lingo Chat/Controllers/ChatsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ChatsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Lingo Chat/Controllers/LoginAndSignup/SignupViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignupViewController.swift
+//  Lingo Chat
+//
+//  Created by Chetan on 2020-08-09.
+//  Copyright © 2020 Chetan. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
